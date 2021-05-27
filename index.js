e2bfd2b3c20d93387fd2788c64968ed7c715076c
SoloLineDefender <72494398+SoloLineDefender@users.noreply.github.com>
2021-05-27 17:33:59 +0300

    chengeAll

--- index.js
+++ index.js
@@ -7,25 +7,42 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
 app.get('/',function(req,res){
-    res.render("index");
+    res.render("main");
 });
 
 app.get('/about',function(req,res){
     res.render("about");
 });
 
+app.get('/login',function(res,req){
+    res.render('login')
+})
+
+app.get('/reg',function(res,req){
+    res.render('reg')
+})
+
+app.get('/shop',function(req,res){
+    res.render("shop");
+})
+
+
 app.get('/news/:id',function(req,res){
     var obj = {title: "новость", id: 4, paragraphs: ['параграф', 'обычный текст', 'Числа: 1, 2, 4', 99]}
     res.render('news', {newsId: req.params.id, obj: obj});
 });
 
-app.get('/index',function(req,res){
-    res.render("index");
+app.get('/main',function(req,res){
+    res.render("main");
 });
 
 app.get('/:else',function(req,res){
-    res.sendFile(__dirname+"/er404.html");
+    res.render("er404");
 });
+
+
+
+
 
 
 app.listen(3000);
